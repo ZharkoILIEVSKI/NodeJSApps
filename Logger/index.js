56d303c4d80897d77dc4cd4f913280a91e3b3d3d
ZharkoILIEVSKI <zarko.ilievski@gmail.com>
2023-11-02 22:59:46 +0100

    logger - 2 faza

--- Logger/index.js
+++ Logger/index.js
@@ -1,11 +1,23 @@
 const { EventEmitter } = require('stream');
 const logEvent = require('./logEvents');
-
+const fs = require('fs');
 const event = require('events');
+const path = require('path');
+
+console.log('----START----');
+console.log(__dirname);
+
+fs.mkdirSync(path.join(__dirname,'logs'));
+fs.writeFileSync(path.join(__dirname,'logs','eventLogs.txt'),'PROBA TEST\n');
 
 class MyEvent extends EventEmitter{};
 const myEvent = new MyEvent();
 
-myEvent.on('log', (msg) => logEvent(msg));
+myEvent.on('log', (msg) => {
+    logEvent(msg);
+    console.log('Presretnuvanje...')
+});
 
 myEvent.emit('log', 'Log just entered!!!');
+
+
--- Logger/logEvents.js
+++ Logger/logEvents.js
@@ -5,22 +5,20 @@ const path = require('path');
 const fsPromises = require('fs').promises;
 
 
-const logEvent = async (message) => {
+const logEvent = async (message) =>
+ {
     const dateTime = format(new Date(), 'dd-MM-yyyy\tHH:mm:ss');
     const id = uuid();
     const log = `${id}\t${dateTime}\t${message}\n`; 
+    console.log(log);
 
-    try {
-        if(!fs.existsSync(path.join(__dirname,'logs'))){
-            fs.mkdir(path.join(__dirname,'logs'));
-        }
-        await fsPromises.appendFile(path.join(__dirname,'logs', 'logEvent.txt'),log, err => {
-            if(err) throw err;
-            console.log("Log witten...");
-        });
-    } catch(error){
-        console.log(error);
-    }
+        if(!fs.exist(path.join(__dirname,'logs'))){
+            await fs.mkdir(path.join(__dirname,'logs'));
+        } else{
+                await fsPromises.appendFile(path.join(__dirname,'logs', 'eventLogs.txt'),log, err => {
+                    if(err) throw err;
+                    console.log("Log witten...");
+        })};
 }
 
 module.exports = logEvent;
